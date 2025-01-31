bd7418634a6c823ab8efa5149b304c2d5d385653
ayk5127 <ayk_5127.spectre@outlook.jp>
2025-02-01 07:19:34 +0900

    ToDoアイテム追加機能の実装

--- todoapp/src/main/java/com/example/todoapp/controller/TodoController.java
+++ todoapp/src/main/java/com/example/todoapp/controller/TodoController.java
@@ -6,21 +6,29 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.todoapp.model.Todo;
 
 @Controller
 @RequestMapping("/todolist")
 public class TodoController {
+	List<Todo> todolist = new ArrayList<>();
 	
 	@GetMapping
 	public String getTodoList(Model model) {
-		List<Todo> todolist = new ArrayList<>();
-	    todolist.add(new Todo(1, "買い物をする", false));
-	    todolist.add(new Todo(2, "本を読む", true));
-	    
 	    model.addAttribute("todolist", todolist);
 	    return "todo-list";
+	}
+	
+	@PostMapping("/add")
+    public String addTodo(@RequestParam String title, Model model) {
+		Todo newTodo = new Todo(todolist.size() + 1, title, false);
+		todolist.add(newTodo);
+
+		model.addAttribute("todolist", todolist);
+		return "redirect:/todolist";
 	}
 }
